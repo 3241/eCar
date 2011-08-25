dd670df15a5c2317ef132360ddd707ce6ea8d341
Sheyne <me@sheyne.com>
2011-08-25 17:56:55 -0600

    Yup, removing /then/ adding main.py

--- main.py
+++
@@ -1,3 +0,0 @@
-"""This will be the main scrip that is run when driving the car. This is so I can make sure commiting is working right."""
-
-print "For Sheyne's testing purposes this needs to print something."
