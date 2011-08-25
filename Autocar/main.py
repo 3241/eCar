b245949ff2c95a952ffe8a2169941d7b68a62d05
Sheyne <me@sheyne.com>
2011-08-25 17:52:26 -0600

    Test commit from Xcode.

--- Autocar/main.py
+++ Autocar/main.py
@@ -1,3 +1,3 @@
-"""This will be the main scrip that is run when driving the car."""
+"""This will be the main scrip that is run when driving the car. This is so I can make sure commiting is working right."""
 
 print "For Sheyne's testing purposes this needs to print something."
