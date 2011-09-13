31d4a207046826cc707819a1cf8f4d0c7abe0111
Sheyne <me@sheyne.com>
2011-09-13 12:29:25 -0600

    Grammar.

--- Robot/IO.py
+++ Robot/IO.py
@@ -3,6 +3,6 @@ class IO:
 	def __init__(self, interpreter, devices):
 		"""Takes file handles, or some way of referencing all of the the devices it will comunicate with. It will initialize connections to each of the devices and listen for data. Also sets the objects's interpreter field to the ones that is passed as an argument."""
 	def received_data(self,source, data):
-		"""Takes data from a given source and passes it to the appropriate method in the interpreter. """
+		"""Takes data from a given source and passes it to the appropriate method in the interpreter. (`received_ultrasonic`, `received_top_camera`, or `received_kinect`)"""
 	def motor_speed(left=None, right=None):
 		"""Sets the left and/or right speed. If either are none, ignore them."""
