63a911f898975ba537b7367a62b345920da220ba
Sheyne <me@sheyne.com>
2011-09-13 17:23:30 -0600

    Romoved old files

--- Robot/Controller.py
+++
@@ -1,7 +0,0 @@
-class Controller:
-	def received_obstacle(self, obstacle):
-		"""Respond to obstacle by varying path."""
-	def received_position(self, position):
-		"""Report current position to server. Adjust course based on most accurate current position/orientation."""
-	def ultrasonic_distance(self, distance):
-		"""Perform the appropriate response for the distance returned."""
--- Robot/IO.py
+++
@@ -1,8 +0,0 @@
-class IO:
-	"""This class deals with input and output with the robot for communications over USB. This includes the microchip, the camera, and the kinect."""
-	def __init__(self, interpreter, devices):
-		"""Takes file handles, or some way of referencing all of the the devices it will comunicate with. It will initialize connections to each of the devices and listen for data. Also sets the objects's interpreter field to the ones that is passed as an argument."""
-	def received_data(self,source, data):
-		"""Takes data from a given source and passes it to the appropriate method in the interpreter. (`received_ultrasonic`, `received_top_camera`, or `received_kinect`)"""
-	def motor_speed(left=None, right=None):
-		"""Sets the left and/or right speed. Only set speeds that are not None."""
