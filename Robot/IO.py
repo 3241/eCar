ebfeef377f76d24acb789efeab278f359800b876
Sheyne <me@sheyne.com>
2011-09-13 11:22:35 -0600

    Filled out basic template for the IO class of the Robot module.

---
+++ Robot/IO.py
@@ -0,0 +1,8 @@
+class IO:
+	"""This class deals with input and output with the robot for communications over USB. This includes the microchip, the camera, and the kinect."""
+	def __init__(self, interpreter, devices):
+		"""Takes file handles, or some way of referencing all of the the devices it will comunicate with. It will initialize connections to each of the devices and listen for data. Also sets the objects's interpreter field to the ones that is passed as an argument."""
+	def received_data(self,source, data):
+		"""Takes data from a given source and passes it to the appropriate method in the interpreter. """
+	def motor_speed(left=None, right=None):
+		"""Sets the left and/or right speed. If either are none, ignore them."""
