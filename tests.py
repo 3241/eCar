070623ef4356bd1575ec1afa88f83ddf0150fb7a
Sheyne <me@sheyne.com>
2011-09-13 17:32:26 -0600

    Fixed glitch in robot.py

--- tests.py
+++ tests.py
@@ -5,3 +5,11 @@ import robot
 controller=robot.Controller()
 controller.interpreter=robot.Interpreter()
 controller.io=robot.IO("References to the cameras and chip")
+
+#this should imply that if:
+io=controller.io
+interpreter=controller.interpreter
+#then
+print io.controller==controller
+print interpreter.controller==controller
+#evaluate to True.
