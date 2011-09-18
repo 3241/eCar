25636e65e20148e5fb9fdd71dc52ae9c503c2a67
Sheyne <me@sheyne.com>
2011-09-17 21:14:50 -0600

    Modified kinect toys

---
+++ kinect toys/examples from libfreenect pure cv (no SimpleCV)/CMakeFiles/cython_freenect.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/sheyne/Dropbox/Dev/libfreenect/wrappers/python/freenect.c" "/Users/sheyne/Dropbox/Dev/libfreenect/wrappers/python/CMakeFiles/cython_freenect.dir/freenect.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ kinect toys/examples from libfreenect pure cv (no SimpleCV)/CMakeFiles/cython_freenect.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cython_freenect.dir/freenect.o"
+  "freenect.c"
+  "freenect.pdb"
+  "freenect.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/cython_freenect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
