06625021e17869a75210003b2aabb340438c5289
Sheyne <me@sheyne.com>
2011-11-01 13:05:57 -0600

    Created a basis for the botmodule class.

--- robot.py
+++ robot.py
@@ -1,23 +1,28 @@
 #! /bin/bash
 
 class BotModule(object):
-    pass
+    def __init__(self, bot):
+        self.bot = bot
+        self.setup()
+	
+	def setup(self):
+		pass
 
 class IO(BotModule):
-    def __init__(self):
-        pass
+	def setup(self):
+		pass
 
 class Glyph(BotModule):
-    def __init__(self):
-        pass
+	def setup(self):
+		pass
 
 class KinectProcessing(BotModule):
-    def __init__(self):
-        pass
+	def setup(self):
+		pass
 
 class AI(BotModule):
-    def __init__(self):
-        pass
+	def setup(self):
+		pass
 
 class Bot(object):
     pass
