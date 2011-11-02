66f89d26c64516a5de85ca8c99da4d014ce03142
Ian Sohl <docsohl@gmail.com>
2011-11-02 12:08:20 -0600

    Filling out AI

--- robot.py
+++ robot.py
@@ -22,7 +22,33 @@ class KinectProcessing(BotModule):
 
 class AI(BotModule):
 	def setup(self):
-		pass
+	    self.minobjectwidth = 5 #Pixels
+	    self.correctdestinationthreshold = 10 #pixels
+	    self.correctheadingthreshold = 0.087 #Radians
+	    self.correctheading = False
+	    self.currentheading = "Unknown"
+	    self.estimatedheading = "Unknown"
+	def GlyphMove(self,moveto,knownat):
+            self.currentheading = knownat
+            self.estimatedheading = knownat
+            result = self.determineDirection(moveto,knownat)
+            if result == "Left":
+                self.bot.startLeft()
+            elif result == "Right":
+                self.bot.startRight()
+            elif result == "Straight":
+                self.bot.startForward()
+	def determineDirection(self,movetoheading,currentheading):
+            if self.currentheading in range(movetoheading-self.correctheadingthreshold,movetoheading+self.correctheadingthreshold):
+                self.correctheading = True
+                return "Straight"
+            elif self.currentheading > movetoheading:
+                return "Left"
+            elif self.currentheading < movetoheading:
+                return "Right"
+        def receivePillars(self.pillars):
+            """Takes Pillar Array and determines appropriate heading"""
+            
 
 class Bot(object):
     def __init__(self, botmodules = (AI, KinectProcessing, Glyph, IO)):
