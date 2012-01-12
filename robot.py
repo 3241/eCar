a4a7b7f783b0bceebc8d9f6ef5eb8ac586ebbced
Ian Sohl <docsohl@gmail.com>
2012-01-12 14:55:41 -0700

    New Robot code

--- robot.py
+++ robot.py
@@ -1,80 +1,133 @@
 #! /usr/bin/python
-import fifolink
+import time
+import os
+import freenect
+import pyroomba
 
-class BotModule(object):
-    def __init__(self, bot):
-        self.bot = bot
-        self.setup()
-	
-	def setup(self):
-		pass
-		
-class IO(BotModule):
-	def setup(self):
-		self.fifo = fifolink.FifoLink("top_camera.fifo", self.receive_top_cam_data)
-	
-	def receive_top_cam_data(self, message):
-		print "Recieved: %s, from the top camera." % message
-
-class Glyph(BotModule):
-	def setup(self):
-		pass
-
-class KinectProcessing(BotModule):
-	def setup(self):
-		pass
-
-class AI(BotModule):
-	def setup(self):
-	    self.minobjectwidth = 5 #Pixels
-	    self.correctdestinationthreshold = 10 #pixels
-	    self.correctheadingthreshold = 0.087 #Radians
-	    self.correctheading = False
-	    self.currentheading = Bot.getHeading()
-	def determineDirection(self,heading):
-            if self.currentheading in range(-self.correctheadingthreshold,self.correctheadingthreshold):
-                self.correctheading = True
-                return "Straight"
-            elif heading < 0:
-                return "Left"
-            elif heading > 0:
-                return "Right"
-        def groupPillars(self,pillars):
-            """Split pillar array into array of pillar (distance,width) tuples"""
-            #group pillars into 5 object groups
-            groups = []
-            newgroup = []
-            for i in range(0,len(pillars)):
-                if i%5==0 or i==len(pillars)-1:
-                    groups.append(newgroup)
-                    newgroup = []
-                newgroup.append(pillars[i])
-        def receivePillars(self,pillars):
-            """Takes Pillar Array and determines appropriate heading"""
+class AI():
+    def setup(self):
+        self.minobjectwidth = 5 #Pixels
+        self.distancethreshold = 0.10 #m
+        self.floor = [] #Must fill this with measurements!
+        self.correctdestinationthreshold = 10 #pixels
+        self.correctheadingthreshold = 0.087 #Radians
+        self.correctheading = False
+        self.currentheading = 0
+        self.newheading = 0
+        self.power = 100 #Standard (safe) speed
+        self.offset = 0 #Radians
+        self.fieldofview = 0.995 #radians (or 57 degrees)
+        self.pixelsperrad = 640/self.fieldofview #I'm pretty sure
+        self.turnradpersec = 1.05#This is an estimate, really ought to check (based on 1 rev/6 secs)
+        self.mainloop()
+    def mainloop(self):
+        while 1:
             """Check to determine if heading is correct"""
+            f = file(os.path.expanduser('~/Dropbox/newdir.txt'),'r')
+            firstline = f.readline()
+            if not firstline=="None":
+                if firstline=="Stop":
+                    roomba.drive_direct(0,0)
+                else:
+                    self.newheading = float(firstline)
+                f.close()
+                f1 = file(os.path.expanduser('~/Dropbox/newdir.txt'),'w')
+                f1.writelines("None")
+            else:
+                f.close()
             while self.correctheading == False:
                 """If it is not correct, begin turning the bot"""
-                heading = bot.getHeading()
                 result = determineDirection(heading)
                 if result=="Straight":
-                    bot.goForward()
+                    roomba.drive_direct(self.power,self.power)
+                    self.correctheading = True
+                    self.newheading = 0
                 elif result=="Left":
-                    bot.goLeft()
+                    roomba.drive_direct(self.power,-self.power)
+                    time.sleep(0.2)
+                    self.currentheading+=-self.turnradpersec*0.2
                 elif result=="Right":
-                    bot.goRight()
-            """Analyze environment and choose a gap to go through"""
-            groupPillars(pillars)
-                    
+                    roomba.drive_direct(-self.power,self.power)
+                    time.sleep(0.2)
+                    self.currentheading+=self.turnradpersec*0.2
+            pillars = self.get_depth_transposed()
+            self.receivePillars(pillars)
+    def determineDirection(self,heading):
+        if self.currentheading in range(-self.correctheadingthreshold,self.correctheadingthreshold):
+            self.correctheading = True
+            return "Straight"
+        elif heading < 0:
+            return "Left"
+        elif heading > 0:
+            return "Right"
+    def groupPillars(self,pillars):
+        """Split pillar array into an array of pillar closest object distances
+        i.e. Each grouping will display distance of closest object within pillar, or 
+        'None' for no object"""
+        #find closest point on each pillar
+        pillardist = []
+        for pillar in pillars:
+            closedist = None
+            for pixel in range(0,len(pillar)):
+                """Assumably, the pillar will be an array of distance measurements
+                This can be compared to either an known array of floor recession measurements
+                     or an algorithm (assumably exponential) to what it should be based on camera height
+                (Note: measurement tolerences are likely to change, especially when camera bounce is properly
+                     analyzed)"""
+                if pillar[pixel]<self.floor[pixel]-self.distancethreshold:
+                    curdist = pillar[pixel]
+                    if curdist<closedist:
+                        closedist=curdist
+            pillardist.append(closedist)
+        #group pillars into 5-object groups. This can be changed as necessary
+        #Takes average distance value
+        newgroup = []
+        for i in range(0,len(pillardist)):
+            if i%5==0 or i==len(pillardist)-1:
+                total = 0
+                for j in range(0,5):
+                    total+=newgroup[i]
+                average = total/len(newgroup)
+            newgroup.append(average)
+        return newgroup
+            
+    def receivePillars(self,pillars):
+        """Takes Pillar Array and determines appropriate heading"""
+        """Analyze environment and choose a gap to go through"""
+        groupedlist = groupPillars(pillars)
+        #THIS IS TEMPORARY: scroll through pillar list and choose widest gap.
+        #Yes, I realize that it may go in circles, but the short-term hope is that the Kinect's
+        #     field of view will limit that.
+        widestgroup = []
+        tempgroup = []
+        for i in range(0,len(groupedlist)):
+            #For the moment, it will only look for 'None'
+            if groupedlist[i]==None:
+                tempgroup.append(i)
+            else:
+                if len(tempgroup)>len(widestgroup):
+                    #Left-side preference, will fix
+                    widestgroup=tempgroup
+                    tempgroup = []
+                else:
+                    tempgroup = []
+        #Determine pixel offset from center
+        centerofimage = int(len(groupedlist)*5/2) #should be about 320 pixels
+        targetblock = widestgroup[int(len(widestgroup)/2)]
+        targetpixel = targetblock*5
+        pixeloffset = targetpixel-centerofimage #i.e. 'right' is positive
+        #This is a temporary fix for determining movement:
+        if pixeloffset>0 and pixeloffset>self.correctheadingthreshold:
+            roomba.drive_direct(-self.power,self.power)
+        elif pixeloffset<0 and pixeloffset>self.correctheadingthreshold:
+            roomba.drive_direct(self.power,-self.power)
+        else:
+            roomba.drive_direct(self.power,self.power)
+        #Will add expected turning amounts, later.
 
-class Bot(object):
-    def __init__(self, botmodules = (AI, KinectProcessing, Glyph, IO)):
-		self.setup_botmodules(botmodules)
-	
-	def setup_botmodules(self, botmodules):
-		"""Configure the set `self.modules` as instances of the passed `botmodules` argument."""
-		self.modules = set()
-		for module in botmodules:
-			try:
-				self.modules.add(module(bot = self))
-			except TypeError:
-				raise TypeError("passed module: %s, is not a BotModule instance." % module)
+    def get_depth(self):
+        return freenect.sync_get_depth()[0]
+    
+    def get_depth_transposed(self):
+        return get_depth().transpose()    
+
